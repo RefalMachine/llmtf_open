aba4228ae9150c8213fe89891014fa7336cc8872
RefalMachine <tikhomirov.mm@gmail.com>
2025-07-29 15:51:15 +0000

    script fixes

--- calculate_benchmark_greedy.sh
+++
@@ -1,7 +0,0 @@
-python benchmark/calculate_benchmark.py \
---model_dir /workdir/data/models/qwen3/Qwen3-4B \
---gen_config_settings benchmark/config_greedy.json \
---conv_path conversation_configs/qwen3-no-think.json \
---output_dir /workdir/projects/devel/llmtf_open/benchmark/greedy/Qwen3-4B \
---tensor_parallel_size 1 \
---force_recalc
--- calculate_benchmark_greedy_api.sh
+++
@@ -1,6 +0,0 @@
-python benchmark/calculate_benchmark_api.py \
---model_dir /workdir/data/models/qwen3/Qwen3-4B \
---gen_config_settings benchmark/config_greedy.json \
---output_dir /workdir/projects/devel/llmtf_open/benchmark/greedy/Qwen3-4B-new \
---force_recalc \
---add_reasoning_tasks
